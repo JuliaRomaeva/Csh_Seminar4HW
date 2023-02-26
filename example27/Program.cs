1e271d83c6a058027d14ad7e394793c59aa15ef4
JuliaRomaeva <julia.romaeva@gmail.com>
2023-02-26 15:14:16 +0300

    сумма цифр числа функция без использования метода строки

---
+++ example27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число : ");
+int num = int.Parse(Console.ReadLine());
+
+int Sum(int n){
+    int sum = 0;
+        while (n > 0){ 
+        sum = n  % 10 + sum; 
+        n /= 10;
+        }
+    return sum;
+}
+int result = Sum(num);
+Console.WriteLine($"{num} -> {result}");
